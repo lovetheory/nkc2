7926239cf4bbe74c779fad97b85efe14ec16b2ea
ctmakro <ctmakro@gmail.com>
2016-05-10 10:15:29 +0800

    throwify returns

--- nkc_modules/validation.js
+++ nkc_modules/validation.js
@@ -3,7 +3,7 @@ module.paths.push('./nkc_modules'); //enable require-ment for this path
 
 //decide whether a submitted post is legal
 exports.validatePost = function(p){
-  if(!p.c)return 'content not exist';
-  if(p.c.length<6)return 'content too short, 6 chars min';
+  if(!p.c)throw 'content not exist';
+  if(p.c.length<6)throw 'content too short, 6 chars min';
   return true;
 };
