e6e9c4e439be584446864acb5c5cc08f0d98f672
ctmakro <ctmakro@gmail.com>
2016-05-19 15:10:19 +0800

    new packages; eat existing avatars and copy them to new directories.

--- taravaEater.js
+++ taravaEater.js
@@ -11,19 +11,36 @@ var spath = __projectroot + 'resources/newavatar_small'
 fs.ensureDirSync(dpath);
 fs.ensureDirSync(spath);
 
-dir.files(fpath,function(err,files){
-  console.log(files[0]);
+var successcount = 0;
+function success(){
+  successcount++;
+  if(successcount%1000==0){
+    console.log('succeed:' ,successcount);
+  }
+}
 
-  files.forEach(function(path){
+dir.files(fpath,function(err,files){
+  console.log('total:',files.length);
+
+  for(f in files){
+
+    var path = files[f]
     var filename =
     path.replace(/.*[\\\/]([0-9_a-z]*\.[a-z]{0,5})/,'$1')
 
     if(/^[0-9]*\.jpg$/.test(filename)){
       fs.copy(path,dpath+'/'+filename,function(err){
         if(err)return console.error(err);
-        console.log('success: '+filename);
+        //console.log('success: '+filename);
+        success()
+      })
+    }else if (/^[0-9]*_small\.jpg$/.test(filename)) {
+      var newfilename = filename.split('_')[0] + '.jpg'
+      fs.copy(path,spath+'/'+newfilename,function(err){
+        if(err) return console.error(err);
+        success()
       })
     }
+  }
 
-  })
 })
