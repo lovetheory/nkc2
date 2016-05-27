6631d91700118421bd9a9196791da4f0e0c59baf
ctmakro <ctmakro@gmail.com>
2016-05-27 16:21:45 +0800

    check on submitted content.

--- nkc_modules/validation.js
+++ nkc_modules/validation.js
@@ -3,7 +3,18 @@ module.paths.push('./nkc_modules'); //enable require-ment for this path
 
 //decide whether a submitted post is legal
 exports.validatePost = function(p){
+
   if(!p.c)throw 'content not exist';
+  if((typeof p.c)!== 'string') throw 'wrong content type, accept string'
   if(p.c.length<6)throw 'content too short, 6 chars min';
+
+  if(p.t!==null && p.t!==undefined){
+    if((typeof p.t)!== 'string') throw 'title wrong type'
+  }
+
+  if(p.l!==null && p.l!==undefined){
+    if((typeof p.l)!== 'string') throw 'wrong lang type, accept string'
+  }
+
   return true;
 };
