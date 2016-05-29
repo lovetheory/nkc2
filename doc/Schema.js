a5f7cde65ad7afed00f2b3a4188822fe63f3a764
ctmakro <ctmakro@gmail.com>
2016-05-30 01:00:23 +0800

    schema updated

--- doc/Schema.js
+++ doc/Schema.js
@@ -6,30 +6,52 @@ var user=
   toc:Number, //创建时间
   tlv:Number, //最后访问（登陆）时间
   username:String, //用户名
-  password:String, //密码（明文）
 
+  count_posts:5,
+  count_threads:0,
+
+  kcb:300,
+  xsf:1, //or null
+
+  certs:['default','moderator'] //证书，即权限组
+  cart:[ //管理车
+    'threads/12313',
+    'posts/534134',
+  ]
+}
+
+var user_personal={
   email:String, //电子邮件地址
   regcode:String, //注册码
 
-  certs:[String,String] //证书，即权限组
-  cart:[ //管理车
-    {itemtype:'thread', id:'12'},
-    {itemtype:'thread', id:'13'},
-  ]
+  hashtype:'pw9'
+
+  //password:String, //密码（明文）
+  password:{
+    salt:'341424',
+    hash:'424515245145145155145',
+  }
 }
 
 var post={ //post 表示楼
   toc:Number, //创建时间
   tlm:Number, //最后修改时间
+
   tid:String, //所属的thread的_key属性
 
   // 以下两个属性是发帖时附上的
   uid:String, //发帖人user的_key
-  username:String, //发帖人user的username属性。根据情况可省略。
+
+  r:['12313','565436'], //附件列表
 
   c:String, //楼的内容
-  t:String, //楼的标题，可以为空字符串
-  l:String, //语言，可能的值是 markdown 或 bbcode
+  t:String, //楼的标题，可以为空字符串或null
+  l:String, //语言，可能的值是 markdown 或 pwbb
+
+  ipoc:'',
+  iplm:'',
+
+  uidlm:'',
 }
 /*
 关于histories
@@ -39,30 +61,43 @@ var post={ //post 表示楼
 
 var thread= //thread表示帖
 {
+  fid:String, //所属的forum的_key属性
+  category:'94', //why is this even important
+
+  //以下属性是发布时或者回帖时更新的。
   toc:Number,
   tlm:Number,
-  fid:String, //所属的forum的_key属性
 
-  //以下两个属性是发布时或者回帖时更新的。
-  lm:post, //最后一个回帖
-  oc:post, //楼主位
+  digest:Boolean,
+
+  count_hit:48,
+  count:1,
+  count_today:0,
+
+  uid:'46528',
+
+  //pid
+  lm:'31241', //最后一个回帖
+  oc:'24124', //楼主位
 }
 
 var forum= //版
 {
-  posts_today:2,
-  posts_total:600,
-  threads_total:63,
+  class:'regular',//内容控制级别
+
+  type:'forum',//类型：真实板块
+  // 'category' //类型：板块分类
+
+  parentid:5, //从属分类
+
+  order:2, //排序
+  count_posts:999,
+  count_threads:60,
+  count_posts_today:6,
+
   display_name:"板块的显示名称",
-  owners:[ //所有者，即：版主
-    {
-      uid:String,
-      username:String,
-    },
-    {
-      //可以有多个
-    }
-  ],
+  description:'简介',
+  owners:String, //csv
 }
 
 var counter={ //计数器
