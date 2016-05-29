398c928a1b034dbb6078427b78682750c5ea9ec6
ctmakro <ctmakro@gmail.com>
2016-05-29 08:33:03 +0800

    new permission model

--- nkc_modules/permissions.js
+++ nkc_modules/permissions.js
@@ -8,19 +8,32 @@ var permissions = {};
 //证书，每张证书将包含不同的权限
 var certificates={
   dev:{
+    display_name:'运维',
+    inheritFrom:['moderator'],
+
     permittedOperations:operations.listAll(), //grandmaster
   },
 
   editor:{
+    display_name:'编辑',
     inheritFrom:['moderator'],
+
+    content_class:{
+      administrative:true,
+    },
+
     permittedOperations:{
       viewQuestions:true,
-      viewClassifiedContent:true,
     }
   },
 
   moderator:{
+    display_name:'版主',
     inheritFrom:['scholar'],
+
+    content_class:{
+      classified:true,
+    },
 
     permittedOperations:{
       addThreadToCart:true,
@@ -28,24 +41,28 @@ var certificates={
 
       removePost:true,
       moveThread:true,
-
-
-      viewClassifiedContent:true,
     }
   },
 
   scholar:{ //学者，1学分为限。
+    display_name:'学者',
     inheritFrom:['default'],
 
-    permittedOperations:{
+    content_class:{
+      sensitive:true,
+    },
 
-      viewSensitiveContent:true,
+    permittedOperations:{
     }
   },
 
   default:{
+    display_name:'学生',
     inheritFrom:['visitor'],
 
+    content_class:{
+      regular:true,
+    },
     permittedOperations:{
       listCart:true,
       clearCart:true,
@@ -61,6 +78,9 @@ var certificates={
   },
 
   visitor:{ //public
+    content_class:{
+      null:true,
+    },
     permittedOperations:{
       viewThread:true,
       viewForum:true,
@@ -81,16 +101,19 @@ var certificates={
   },
 
   banned:{
+    display_name:'封禁',
+
     inheritFrom:['visitor'],
     permittedOperations:{
       viewExam:false,
-    }
+    },
   },
 };
 
 //certs is [] of certificate names
 var getPermissionsFromCerts = (certsArray)=>{
-  var permissionsOfCerts={};
+  var permittedOperations={};
+  var content_class={};
 
   for(i in certsArray)
   {
@@ -101,17 +124,24 @@ var getPermissionsFromCerts = (certsArray)=>{
 
     //recursive inheritance.
     if(certificate.inheritFrom){
-      Object.assign(permissionsOfCerts,getPermissionsFromCerts(certificate.inheritFrom))
+      var c = getPermissionsFromCerts(certificate.inheritFrom)
+
+      Object.assign(permittedOperations,c.permittedOperations)
+      Object.assign(content_class,c.content_class)
     }
 
-    Object.assign(permissionsOfCerts,certificate.permittedOperations)
+    Object.assign(permittedOperations,certificate.permittedOperations)
+    Object.assign(content_class,certificate.content_class)
   }
-  return permissionsOfCerts
+  return {
+    permittedOperations,
+    content_class,
+  }
 };
 
 permissions.getPermissionsFromCerts = getPermissionsFromCerts
 
-permissions.getPermissionListFromUser = function(user){
+permissions.getPermissionsFromUser = function(user){
   if(!user)return getPermissionsFromCerts(['visitor'])
   if(!user.certs)return getPermissionsFromCerts(['default'])
   return getPermissionsFromCerts(user.certs)
