85404738198a28a6c9edcea7b6ee4df1022c1543
unknown <>
2017-04-01 17:14:40 +0800

    fixed index gallery

--- nkc_modules/api_operations/gallery.js
+++ nkc_modules/api_operations/gallery.js
@@ -15,29 +15,49 @@ var layer = require('../layer')
 table.getGalleryRecent = {
   operation:function(params){
     return AQL(`
-      for p in posts
-      sort p.toc desc
-      filter p.r
-      filter length(p.r)
-      filter !p.disabled
-
-      let parr = (
-        for r in p.r
-        let res = document(resources,r)
-        filter position(['jpg','png','svg','jpeg'],res.ext,false)
-        return res
-      )
-      filter length(parr)
-
-      let thread = document(threads,p.tid)
-      let oc = document(posts,thread.oc)
-      let ocuser = document(users,oc.uid)
-      let forum = document(forums,thread.fid)
-      filter forum.visibility == true && forum._key != '97'//去掉自由市场
-
-      limit 6
-
-      return {r:parr[rand()*length(parr)],forum,thread:merge(thread,{oc,ocuser})}
+      FOR t IN threads
+        FILTER t.fid != 'recycle' && t.fid != '97' && t.disable != true
+        SORT t.toc DESC
+        LET p = DOCUMENT(posts, t.oc)
+        FILTER p.r && LENGTH(p.r)
+        LET parr = (
+          FOR r IN p.r
+            LET res = DOCUMENT(resources, r)
+            FILTER POSITION(['jpg','png','svg','jpeg'], res.ext, false)
+            RETURN res
+        )
+        FILTER LENGTH(parr)
+        LET user = DOCUMENT(users, t.uid)
+        LET forum = DOCUMENT(forums, t.fid)
+        FILTER forum.visibility == true
+        LIMIT 6
+        RETURN {
+          r: parr[RAND() * LENGTH(parr)],
+          forum,
+          thread: MERGE(t, {oc: p, ocuser: user})
+        }
+        
+      // filter p.r
+      // filter length(p.r)
+      // filter !p.disabled
+      //
+      // let parr = (
+      //   for r in p.r
+      //   let res = document(resources,r)
+      //   filter position(['jpg','png','svg','jpeg'],res.ext,false)
+      //   return res
+      // )
+      // filter length(parr)
+      //
+      // let thread = document(threads,p.tid)
+      // let oc = document(posts,thread.oc)
+      // let ocuser = document(users,oc.uid)
+      // let forum = document(forums,thread.fid)
+      // filter forum.visibility == true && forum._key != '97'//去掉自由市场
+      //
+      // limit 6
+      //
+      // return {r:parr[rand()*length(parr)],forum,thread:merge(thread,{oc,ocuser})}
       `
     )
     .then(arr=>{
