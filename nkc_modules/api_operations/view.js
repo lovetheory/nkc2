4496ae16615f5207f5bc98af3da0185bf624888b
AAA <p_xigui@163.com>
2017-09-18 11:55:15 +0800

    添加查询username

--- nkc_modules/api_operations/view.js
+++ nkc_modules/api_operations/view.js
@@ -2697,6 +2697,7 @@ table.viewBehaviorLogs = {
             username: userTo.username
           }
         }
+        
         let newPage = new layer.Paging(page).getPagingParams(result.length);
         newPage.page = params.page || 1;
         data.page = newPage;
