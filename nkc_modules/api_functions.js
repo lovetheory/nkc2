3cdc64dc2e23d4cdc909b44f8d3be10ae3b21195
AAA <p_xigui@163.com>
2017-09-22 17:55:48 +0800

    添加A卷考试入

--- nkc_modules/api_functions.js
+++ nkc_modules/api_functions.js
@@ -449,9 +449,9 @@ apifunc.exam_gen = function(options){
       return layer.Question.randomlyListQuestionsOfCategory(category,settings.exam.number_of_questions_subjective,seed+1)
       .then(arr=>{
         qarr = qarr.concat(arr)
-        /*if(category == 'mix'){
+        if(category == 'mix'){
           return layer.Question.randomlyListQuestionsOfCategory('common',settings.exam.number_of_questions_common_a,seed+2);
-        }*/
+        }
         return layer.Question.randomlyListQuestionsOfCategory('common',settings.exam.number_of_questions_common,seed+2)
       })
       .then(arr=>{
