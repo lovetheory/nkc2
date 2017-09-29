97446f999c1d459baa868144e6114706e796adb4
AAA <p_xigui@163.com>
2017-09-29 15:02:03 +0800

    增加绑定手机号发送手机验证码，不在借用修改密码的验证码发送接口

--- nkc_modules/mailSecrets.js
+++ nkc_modules/mailSecrets.js
@@ -15,12 +15,13 @@ module.exports = {
     }
   },
 
-  sendSMS: function(phone, code, fn, callback){
+  sendSMS: function(phone, code, fn, callback){\
+    var period = 15*60*1000;
     if(fn === 'register'){
       //注册用户走这里
       new alidayu('23632480', 'e264a8555d0ef54a9e881969572ed4d2').smsSend({
         sms_free_sign_name: '科新社',  //短信签名
-        sms_param: {"code": code, "product": "科创论坛"},
+        sms_param: {"code": code, "period": period},
         rec_num: phone,
         sms_template_code: 'SMS_100865101',//注册模板
         sms_type: 'normal'
@@ -30,7 +31,7 @@ module.exports = {
       //修改密码走这里
       new alidayu('23632480', 'e264a8555d0ef54a9e881969572ed4d2').smsSend({
         sms_free_sign_name: '科新社',  //短信签名
-        sms_param: {"code": code, "product": "科创论坛"},
+        sms_param: {"code": code, "period": period},
         rec_num: phone,
         sms_template_code: 'SMS_100810088',//大于平台手机找回密码模板号
         sms_type: 'normal'
@@ -40,7 +41,7 @@ module.exports = {
       //现有账号实名认证 绑定手机号
       new alidayu('23632480', 'e264a8555d0ef54a9e881969572ed4d2').smsSend({
         sms_free_sign_name: '科新社',  //短信签名
-        sms_param: {"code": code, "product": "科创论坛"},
+        sms_param: {"code": code, "period": period},
         rec_num: phone,
         sms_template_code: 'SMS_100755081',
         sms_type: 'normal'
