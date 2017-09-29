4264aaa3cb5f49640c8432431474f9d9a4308cc7
Li Zheng <lzszone@gmail.com>
2017-09-29 11:13:15 +0800

    add

---
+++ nkc_modules/mailSecrets.js
@@ -0,0 +1,43 @@
+//this is a template for mailSecrets.js
+
+//copy, rename to 'mailSecrets.js' then tweak the settings
+
+var alidayu = require('alidayu-node');
+
+module.exports = {
+  smtpConfig:{
+    host: 'smtp.exmail.qq.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: 'it@kc.ac.cn',
+      pass: 'ccD711q'
+    }
+  },
+
+  sendSMS: function(phone, code, fn, callback){
+    if(fn === 'register'){
+      //注册用户走这里
+      new alidayu('23632480', 'e264a8555d0ef54a9e881969572ed4d2').smsSend({
+        sms_free_sign_name: '注册验证',  //短信签名
+        sms_param: {"code": code, "product": "科创论坛"},
+        rec_num: phone,
+        sms_template_code: 'SMS_46565069',//注册模板
+        sms_type: 'normal'
+      }, callback)
+    }
+    else if(fn === 'reset'){
+      //修改密码走这里
+      new alidayu('23632480', 'e264a8555d0ef54a9e881969572ed4d2').smsSend({
+        sms_free_sign_name: '身份验证',  //短信签名
+        sms_param: {"code": code, "product": "科创论坛"},
+        rec_num: phone,
+        sms_template_code: 'SMS_46625024',//大于平台手机找回密码模板号
+        sms_type: 'normal'
+      }, callback)
+    }
+    else throw '错误的sms调用方式'
+  },
+
+  senderString:'"中国科创联互联网中心" <it@kc.ac.cn>'
+}
